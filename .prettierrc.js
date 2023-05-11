67a0f27c6338bae13a2de705a3634d8ded5f1a42
kohdice <kohdice@gmail.com>
2023-05-11 19:15:49 +0900

    Add config files

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  importOrder: ['^react$|^react/|^next$|^next/', '<THIRD_PARTY_MODULES>', '^[@/]|^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
